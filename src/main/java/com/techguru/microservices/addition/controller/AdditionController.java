5fd855fd53a06d0e8d82b409209fcbafbe9c95ca
gurubaladhinesh <gurubaladhinesh@gmail.com>
2024-07-21 15:56:32 +0530

    code changes

--- src/main/java/com/techguru/microservices/addition/controller/AdditionController.java
+++ src/main/java/com/techguru/microservices/addition/controller/AdditionController.java
@@ -2,19 +2,21 @@ package com.techguru.microservices.addition.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
+@RequestMapping("/add")
 public class AdditionController {
-
 
     @GetMapping
     public ResponseEntity<Double> add(@RequestParam List<Double> n) {
         double result = 0.0;
-        if (n != null) {
+        if (Objects.nonNull(n)) {
             result = n.stream().mapToDouble(x -> x).sum();
         }
         return ResponseEntity.ok(result);
